4f2dbe45d737e2a367ee41a11ea2f1564a674f13
tenbuschs <simon@tenbusch.de>
2025-06-22 15:54:36 +1200

    create ExpandableEventTile widget

--- lib/timeline.dart
+++ lib/timeline.dart
@@ -6,10 +6,7 @@ import 'l10n/app_localizations.dart';
 class Timeline extends StatelessWidget {
   final Map<String, List<CvEvent>> groupedEvents;
 
-  const Timeline({
-    super.key,
-    required this.groupedEvents,
-  });
+  const Timeline({super.key, required this.groupedEvents});
 
   @override
   Widget build(BuildContext context) {
@@ -50,127 +47,7 @@ class Timeline extends StatelessWidget {
                 ),
               ),
               // Expandable event tiles
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      entry.key,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    ...entry.value.map((exp) {
-                      final key = GlobalKey();
-                      eventKeys[exp.id] = key;
-                      return Container(
-                        key: key,
-                        margin: const EdgeInsets.only(bottom: 16),
-                        child: ExpansionTile(
-                          collapsedBackgroundColor: Colors.grey[900],
-                          backgroundColor: Colors.grey[850],
-                          leading: _categoryIcon(exp.category),
-                          title: Text(
-                            exp.title,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          subtitle: Text(
-                            '${formatPeriod(exp.startDate, exp.endDate, context)} • ${exp.company} • ${exp.location}',
-                          ),
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                bottom: 16.0,
-                                top: 16.0,
-                                left: 16,
-                                right: 16,
-                              ),
-                              child: Text(
-                                exp.description,
-                                style: const TextStyle(fontSize: 16),
-                              ),
-                            ),
-                            if (exp.highlights.isNotEmpty)
-                              Padding(
-                                padding: const EdgeInsets.only(bottom: 16.0, left: 16),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      'Highlights:',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    ...exp.highlights.map(
-                                      (h) => Row(
-                                        children: [
-                                          const Text(
-                                            '• ',
-                                            style: TextStyle(fontSize: 16),
-                                          ),
-                                          Expanded(child: Text(h)),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            if (exp.references.isNotEmpty)
-                              Padding(
-                                padding: const EdgeInsets.only(bottom: 16.0, left: 16),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      'Reference on request:',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    ...exp.references.map(
-                                          (h) => Row(
-                                        children: [
-                                          const Text(
-                                            '• ',
-                                            style: TextStyle(fontSize: 16),
-                                          ),
-                                          Expanded(child: Text(h)),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            if (exp.imageAssets.isNotEmpty)
-                              SizedBox(
-                                height: 160,
-                                child: PageView.builder(
-                                  itemCount: exp.imageAssets.length,
-                                  itemBuilder:
-                                      (context, index) => Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                          horizontal: 16.0,
-                                          vertical: 15.0,
-                                        ),
-                                        child: Image.asset(
-                                          exp.imageAssets[index],
-                                        ),
-                                      ),
-                                ),
-                              ),
-                          ],
-                        ),
-                      );
-                    }),
-                  ],
-                ),
-              ),
+              ExpandableEventTile(entry: entry),
             ],
           ),
         );
@@ -185,11 +62,13 @@ String formatPeriod(String startDate, String? endDate, BuildContext context) {
   final endDt = endDate == null ? DateTime.now() : DateTime.parse(endDate);
 
   final start = DateFormat.yMMM(locale).format(startDt);
-  final endStr = endDate == null
-      ? AppLocalizations.of(context)!.currentlyWorking
-      : DateFormat.yMMM(locale).format(endDt);
+  final endStr =
+      endDate == null
+          ? AppLocalizations.of(context)!.currentlyWorking
+          : DateFormat.yMMM(locale).format(endDt);
 
-  int totalMonths = (endDt.year - startDt.year) * 12 + (endDt.month - startDt.month);
+  int totalMonths =
+      (endDt.year - startDt.year) * 12 + (endDt.month - startDt.month);
   if (endDt.day < startDt.day) totalMonths--;
 
   final years = totalMonths ~/ 12;
@@ -207,28 +86,17 @@ String formatPeriod(String startDate, String? endDate, BuildContext context) {
 
   return '$start – $endStr ($duration)';
 }
+
 // Function to return an icon based on the event category
 Icon _categoryIcon(String category) {
   // This function returns different icons based on the category
   switch (category) {
     case 'job':
-      return const Icon(
-        Icons.work,
-        color: Color(0xFF2D6045),
-        size: 30,
-      );
+      return const Icon(Icons.work, color: Color(0xFF2D6045), size: 30);
     case 'part-time':
-      return const Icon(
-        Icons.work,
-        color: Color(0xFF2D6045),
-        size: 30,
-      );
+      return const Icon(Icons.work, color: Color(0xFF2D6045), size: 30);
     case 'seasonal':
-      return const Icon(
-        Icons.av_timer,
-        color: Color(0xFF2D6045),
-        size: 30,
-      );
+      return const Icon(Icons.av_timer, color: Color(0xFF2D6045), size: 30);
     case 'uni':
       return const Icon(
         Icons.school,
@@ -248,10 +116,103 @@ Icon _categoryIcon(String category) {
         size: 30,
       );
     default:
-      return const Icon(
-        Icons.work,
-        color: Color(0xFF2D6045),
-        size: 30,
-      );
+      return const Icon(Icons.work, color: Color(0xFF2D6045), size: 30);
+  }
+}
+
+
+
+
+// class for the expandable event tile
+class ExpandableEventTile extends StatelessWidget {
+  final MapEntry<String, List<CvEvent>> entry;
+
+  const ExpandableEventTile({super.key, required this.entry});
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, GlobalKey> eventKeys = {};
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ...entry.value.map((exp) {
+            final key = GlobalKey();
+            eventKeys[exp.id] = key;
+            return Container(
+              key: key,
+              margin: const EdgeInsets.only(bottom: 16),
+              child: ExpansionTile(
+                collapsedBackgroundColor: Colors.grey[900],
+                backgroundColor: Colors.grey[850],
+                leading: _categoryIcon(exp.category),
+                title: Text(
+                  exp.title,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  '${formatPeriod(exp.startDate, exp.endDate, context)} • ${exp.company} • ${exp.location}',
+                ),
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 16.0,
+                    ),
+                    child: Text(
+                      exp.description,
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  if (exp.highlights.isNotEmpty)
+                    _bulletSection('Highlights:', exp.highlights),
+                  if (exp.references.isNotEmpty)
+                    _bulletSection('Reference on request:', exp.references),
+                  if (exp.imageAssets.isNotEmpty)
+                    SizedBox(
+                      height: 160,
+                      child: PageView.builder(
+                        itemCount: exp.imageAssets.length,
+                        itemBuilder:
+                            (context, index) => Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0,
+                                vertical: 15.0,
+                              ),
+                              child: Image.asset(exp.imageAssets[index]),
+                            ),
+                      ),
+                    ),
+                ],
+              ),
+            );
+          }),
+        ],
+      ),
+    );
+  }
+
+  Widget _bulletSection(String title, List<String> items) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0, left: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+          ...items.map(
+            (item) => Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('• ', style: TextStyle(fontSize: 16)),
+                Expanded(child: Text(item)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
